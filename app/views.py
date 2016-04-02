a593872c94a2bc9dd1f2b8a096fa05a4f6b84eb0
QI WANG <Daisy@QIs-MacBook-Pro.local>
2016-04-02 19:13:08 +0100

    03-webform

--- app/views.py
+++ app/views.py
@@ -1,11 +1,14 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from .forms import LoginForm
+
+
 
 @app.route('/')
 @app.route('/index')
 def index():
     user ={ 'nickname': 'Emma'} # fake user
-    posts =[ #fake array of posts
+    posts =[ # fake array of posts
         {
             'author': { 'nickname' : 'Yang'},
             'body': ' I am so clever'
@@ -19,6 +22,22 @@ def index():
                            title='Home',
                            user =user,
                            posts = posts)
+
+# index view function suppressed for brevity
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for OpenID="'+ form.openid.data + '", remember_me='+ str(form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html',
+                           title='Sign In',
+                           form =form,
+                           providers = app.config['OPENID_PROVIDERS'])
+
+
+
 # <html>
 #    <head>
 #      <title> Home Page </title>
--- app/views.py~
+++ app/views.py~
@@ -1,6 +1,30 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user ={ 'nickname': 'Emma'} # fake user
+    posts =[ #fake array of posts
+        {
+            'author': { 'nickname' : 'Yang'},
+            'body': ' I am so clever'
+        },
+        {
+            'author': { 'nickname' : 'Liu'},
+            'body': 'I am so stupid'
+        }
+    ]
+    return render_template("index.html",
+                           title='Home',
+                           user =user,
+                           posts = posts)
+# <html>
+#    <head>
+#      <title> Home Page </title>
+#    </head>
+#    <body>
+#      <h1> Hello, ''' + user['nickname'] + ''' </h1>
+#    </body>
+# </html>
+# '''
