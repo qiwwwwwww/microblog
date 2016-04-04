40c1bb812000e09b70ab2f60125aac556b17f3f8
QI WANG <Daisy@QIs-MacBook-Pro.local>
2016-04-04 22:15:51 +0100

    07-unit testing/but actually fixes bugs

--- config.py
+++ config.py
@@ -8,10 +8,21 @@ OPENID_PROVIDERS =[
     {'name': 'Yahoo', 'url': 'https://me.yahoo.com'},
     {'name': 'AOL', 'url': 'http://openid.aol.com/<username>'},
     {'name': 'Flickr', 'url': 'http://www.flickr.com/<username>'},
-    {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}]
+    {'name': 'URL', 'url': 'login.ubuntu.com'},
+    {'name': 'MyOpenID', 'url': '<username>.myopenid.com'}]
 
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+
+
+# mail server settings
+MAIL_SERVER = 'localhost'
+MAIL_PORT = 25
+MAIL_USERNAME = None
+MAIL_PASSWORD = None
+
+# administrator list
+ADMINS = ['qwictest@gmail.com']
