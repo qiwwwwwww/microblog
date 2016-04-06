9cfceaee462e31ca50e9761caf1248f21eead6c1
QI WANG <Daisy@QIs-MacBook-Pro.local>
2016-04-06 23:43:09 +0100

    13-Dates and Times/everying is fine except the follower is -1? quite strange

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from flask.ext.login import LoginManager
 from flask.ext.openid import OpenID
 from config import basedir, ADMINS, MAIL_SERVER, MAIL_PORT, MAIL_USERNAME, MAIL_PASSWORD
 from flask.ext.mail import Mail
+from .momentjs import momentjs
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -37,5 +38,6 @@ if not app.debug:
     app.logger.info('microblog startup')
 
 mail = Mail(app)
+app.jinja_env.globals['momentjs'] = momentjs
 
 from app import views, models
